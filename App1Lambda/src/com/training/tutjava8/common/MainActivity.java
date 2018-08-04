c0b873de35b30b77bd04d7a430049aeb1473ede9
ajinkyazore <25245619+ajinkyazore@users.noreply.github.com>
2018-08-04 23:03:56 +0530

    Adding lambda

--- App1Lambda/src/com/training/tutjava8/common/MainActivity.java
+++ App1Lambda/src/com/training/tutjava8/common/MainActivity.java
@@ -1,5 +1,25 @@
 package com.training.tutjava8.common;
 
 public class MainActivity {
-
+	public static void main(String[] args) {
+		// lambda expression to implement above
+        // functional interface. This interface
+        // by default implements abstractFun()
+        FuncInterface fobj = (int x)->System.out.println(2*x);
+ 
+        // This calls above lambda expression and prints 10.
+        fobj.abstractFun(5);
+	}
 }
+
+interface FuncInterface
+{
+    // An abstract function
+    void abstractFun(int x);
+ 
+    // A non-abstract (or default) function
+    default void normalFun()
+    {
+       System.out.println("Hello");
+    }
+}	
