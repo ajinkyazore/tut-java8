de6f2bc3e31637d3c16c5b502f7b0a88ae4d1607
ajinkyazore <25245619+ajinkyazore@users.noreply.github.com>
2018-08-06 01:05:48 +0530

    Method Reference Sample

--- App2MethodRef/src/com/training/tutjava8/common/MainActivity.java
+++ App2MethodRef/src/com/training/tutjava8/common/MainActivity.java
@@ -1,7 +1,16 @@
 package com.training.tutjava8.common;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity {
  public static void main(String[] args) {
+	List nameList = new ArrayList<>();
+	nameList.add("Steve Rogers");
+	nameList.add("Steve Rogers");
+	nameList.add("Steve Rogers");
+	
+	nameList.forEach(System.out::println);
 	
 }
 }
