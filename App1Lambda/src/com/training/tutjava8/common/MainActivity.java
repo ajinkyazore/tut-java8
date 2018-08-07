dd67ed77b212439086977b443544a11a24e0e574
ajinkyazore <25245619+ajinkyazore@users.noreply.github.com>
2018-08-07 23:10:18 +0530

    Using map and reduce

--- App1Lambda/src/com/training/tutjava8/common/MainActivity.java
+++ App1Lambda/src/com/training/tutjava8/common/MainActivity.java
@@ -1,5 +1,10 @@
 package com.training.tutjava8.common;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.training.tutjava8.domain.Person;
+
 public class MainActivity {
 	public static void main(String[] args) {
 		// lambda expression to implement above
@@ -9,6 +14,13 @@ public class MainActivity {
  
         // This calls above lambda expression and prints 10.
         fobj.abstractFun(5);
+        
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("Steve Rogers",35));
+        personList.add(new Person("Tony Stark",40));
+        personList.add(new Person("Banner",38));
+        
+        System.out.println(personList.stream().mapToInt(Person::getAge).sum());
 	}
 }
 
