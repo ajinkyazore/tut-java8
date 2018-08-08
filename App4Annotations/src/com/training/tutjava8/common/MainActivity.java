f0924ba43b8d6558dd107f1152d07e6a823a478a
ajinkyazore <25245619+ajinkyazore@users.noreply.github.com>
2018-08-08 22:44:03 +0530

    Adding skeleton function

---
+++ App4Annotations/src/com/training/tutjava8/common/MainActivity.java
@@ -0,0 +1,13 @@
+package com.training.tutjava8.common;
+
+public class MainActivity {
+	
+@SuppressWarnings("unchecked")	
+ public static void main(String[] args) {
+	
+	 String str1 = "";
+	 String str2 = null;
+	 
+
+}
+}
