773e434c4a959a00ce3c0417960073c8cbe0486a
ajinkyazore <25245619+ajinkyazore@users.noreply.github.com>
2018-08-09 08:36:45 +0530

    Adding Annotations

--- App4Annotations/src/com/training/tutjava8/common/MainActivity.java
+++ App4Annotations/src/com/training/tutjava8/common/MainActivity.java
@@ -1,6 +1,10 @@
 package com.training.tutjava8.common;
 
+import org.springframework.lang.NonNull;
+
 public class MainActivity {
+	
+	static @NonNull String str3;
 	
 @SuppressWarnings("unchecked")	
  public static void main(String[] args) {
@@ -8,6 +12,8 @@ public class MainActivity {
 	 String str1 = "";
 	 String str2 = null;
 	 
-
+	  str3 = str2;
+	  
+	  System.out.println(str3);
 }
 }
